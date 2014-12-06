9ea377974bcd1058e292cfe4402ac38084e93953
liudonghua123 <liudonghua123@gmail.com>
2014-12-06 20:47:20 +0800

    add internationalization support

--- js/controller.js
+++ js/controller.js
@@ -1,27 +1,62 @@
 // Bindings go here
 
 
-$(document).ready(function(){
-	var game = new Game();
-	game.beginGame();
+$(document).ready(function () {
 
-	$('.reset').click(function(){
-		$('#restartLevel').modal('show');
-	});
+    i18n = new I18n({
+        directory: "locales",
+        locale: (window.navigator.userLanguage || window.navigator.language || "en-us").toLowerCase(),
+        extension: ".json"
+    });
 
-	$('.newgame').click(function(){
-		$('#newGame').modal('show');
-	});
+    $("#title").text(i18n.__("title"));
+    $("#reset").text(i18n.__("reset"));
+    $("#new").text(i18n.__("new"));
+    $("#instruct").text(i18n.__("instruct"));
 
-	$('#resetLevelConfirm').click(function(){
-		game.onResetLevelClick();
-	});
+    $message_modal = $('#message-modal').modal({
+        backdrop: true,
+        show: false,
+        keyboard: false
+    });
 
-	$('#newGameConfirm').click(function(){
-		game.onNewGameClick();
-	});
+    showMessage = function (header, body, btnSubmitText, callback) {
+        $message_modal
+            .find('.modal-header > h4').text(header).end()
+            .find('.modal-body p').html(body).end();
 
-	$('.instruct').click(function(){
-		$('#instructions').modal('show');
-	})
+        if (btnSubmitText) {
+            $message_modal
+                .find('.callback-btn').html(btnSubmitText).show().end()
+                .find('.callback-btn')
+                .off('click.callback')
+                .on('click.callback', function () {
+                    callback();
+                    $message_modal.modal('hide');
+                }).end();
+        }
+        else {
+            $message_modal.find('.callback-btn').hide();
+        }
+        $message_modal.modal('show');
+    };
+
+    $('.reset').click(function () {
+        showMessage(i18n.__("confirm"), i18n.__("reset_confirm_details"), i18n.__("reset"), function () {
+            game.onResetLevelClick();
+        });
+    });
+
+    $('.newgame').click(function () {
+        showMessage(i18n.__("confirm"), i18n.__("new_confirm_details"), i18n.__("new"), function () {
+            game.onNewGameClick();
+        });
+    });
+
+    $('.instruct').click(function () {
+        showMessage(i18n.__("instruct"), i18n.__("instruct_details"), null, null);
+    });
+
+    var game = new Game();
+    game.beginGame();
 });
--- js/viewmodel.js
+++ js/viewmodel.js
@@ -2,97 +2,97 @@
 
 function cookieHelper() {
 
-	this.readCookie = function(callback) {
-		callback(this.getCookie("gs"));
-	}
+    this.readCookie = function (callback) {
+        callback(this.getCookie("gs"));
+    }
 
-	this.writeCookie = function(game) {
-		var d = new Date();
-		d.setTime(d.getTime() + (300*24*60*60*1000));
-		var expires = "expires="+d.toGMTString();
-		var toWrite = "gs=" + 
-			game.currentClicks + " " +
-			game.bestLevel + " " +
-			game.clicksForBest + " " +
-			game.totalClicks + " " +
-			game.level + " " +
-			game.isFirstGame + " " +
-			game.gb.board.toString() + 
-			"; "+ expires;
-		document.cookie = toWrite;
-	}
+    this.writeCookie = function (game) {
+        var d = new Date();
+        d.setTime(d.getTime() + (300 * 24 * 60 * 60 * 1000));
+        var expires = "expires=" + d.toGMTString();
+        var toWrite = "gs=" +
+            game.currentClicks + " " +
+            game.bestLevel + " " +
+            game.clicksForBest + " " +
+            game.totalClicks + " " +
+            game.level + " " +
+            game.isFirstGame + " " +
+            game.gb.board.toString() +
+            "; " + expires;
+        document.cookie = toWrite;
+    }
 
-	this.getCookie = function(cname) {
-	    var name = cname + "=";
-	    var ca = document.cookie.split(';');
-	    for(var i=0; i<ca.length; i++) {
-	        var c = ca[i];
-	        while (c.charAt(0)==' ') c = c.substring(1);
-	        if (c.indexOf(name) != -1) return c.substring(name.length,c.length);
-	    }
-	    return "";
-	}
+    this.getCookie = function (cname) {
+        var name = cname + "=";
+        var ca = document.cookie.split(';');
+        for (var i = 0; i < ca.length; i++) {
+            var c = ca[i];
+            while (c.charAt(0) == ' ') c = c.substring(1);
+            if (c.indexOf(name) != -1) return c.substring(name.length, c.length);
+        }
+        return "";
+    }
 }
 
 function styleHelper() {
-	this.setGridSize = function(level) {
-		var margin = this.getMargin(level)
-		var res = ($('.container').width() - margin * level) / (level);
-		$('.gamesquare').css('margin-right', margin);
-		$('.gamesquare').css('width', res);
-		$('.gamesquare').css('height', res);
-		$('.gamerow').css('height', res);
-		$('.gamerow').css('margin-right', margin * (-1));
-		$('.gamerow').css('margin-bottom', margin);
-		$('.board').css('padding', margin);
-		$('.board').css('padding-bottom', 0);
-	}
+    this.setGridSize = function (level) {
+        var margin = this.getMargin(level)
+        var res = ($('.container').width() - margin * level) / (level);
+        $('.gamesquare').css('margin-right', margin);
+        $('.gamesquare').css('width', res);
+        $('.gamesquare').css('height', res);
+        $('.gamerow').css('height', res);
+        $('.gamerow').css('margin-right', margin * (-1));
+        $('.gamerow').css('margin-bottom', margin);
+        $('.board').css('padding', margin);
+        $('.board').css('padding-bottom', 0);
+    }
 
-	this.getMargin = function(level) {
-		if (level <= 6) return 15;
-		if (level > 15) return 5;
-		return 20 - level;
-	}
+    this.getMargin = function (level) {
+        if (level <= 6) return 15;
+        if (level > 15) return 5;
+        return 20 - level;
+    }
 }
 
 function cheesyWordGetter() {
-	this.words = ['非常好', '干得漂亮', '帅呆了', '哇塞', 'I服U', '天才诞生了'];
+    this.words = [window.i18n.__("cheer_up_1"), window.i18n.__("cheer_up_2"), window.i18n.__("cheer_up_3"), window.i18n.__("cheer_up_4"), window.i18n.__("cheer_up_5"), window.i18n.__("cheer_up_6")];
 
-	this.getWord = function() {
-		return this.words[Math.floor(Math.random()*this.words.length)];
-	}
+    this.getWord = function () {
+        return this.words[Math.floor(Math.random() * this.words.length)];
+    }
 }
 
 function Game() {
-	var self = this;
+    var self = this;
 
-	// Local game state variables
-	this.currentClicks = 0;
-	this.bestLevel = 1;
-	this.clicksForBest = 0;
-	this.totalClicks = 0;
-	this.level = 1;
-	this.isFirstGame = 1;
+    // Local game state variables
+    this.currentClicks = 0;
+    this.bestLevel = 1;
+    this.clicksForBest = 0;
+    this.totalClicks = 0;
+    this.level = 1;
+    this.isFirstGame = 1;
 
-	// Objects that help facilitate the game
-	this.gb;
-	this.sh = new styleHelper();
-	this.cookh = new cookieHelper();
-	this.cwg = new cheesyWordGetter();
+    // Objects that help facilitate the game
+    this.gb;
+    this.sh = new styleHelper();
+    this.cookh = new cookieHelper();
+    this.cwg = new cheesyWordGetter();
 
-	this.processClick = function(w, h) {
-		this.gb.processClick(w, h);
-		this.currentClicks++;
-		this.totalClicks++;
-		this.updateCounts();
-		if (this.gb.isGameWin()) {
-			this.gameEnd(function(){
-				self.cookh.writeCookie(self);
-			});
-		} else {
-			self.cookh.writeCookie(self);
-		}
-	}
+    this.processClick = function (w, h) {
+        this.gb.processClick(w, h);
+        this.currentClicks++;
+        this.totalClicks++;
+        this.updateCounts();
+        if (this.gb.isGameWin()) {
+            this.gameEnd(function () {
+                self.cookh.writeCookie(self);
+            });
+        } else {
+            self.cookh.writeCookie(self);
+        }
+    }
 
     this.processMouseover = function (w, h) {
         this.gb.processMouseover(w, h);
@@ -102,98 +102,98 @@ function Game() {
         this.gb.processMouseout(w, h);
     }
 
-	this.beginGame = function() {
-		var res;
-		this.cookh.readCookie(function(csv){
-			res = csv;
-			if (res != "") {
-				var state = res.split(" ");
-				self.currentClicks = parseInt(state[0]);
-				self.bestLevel = parseInt(state[1]);
-				self.clicksForBest = parseInt(state[2]);
-				self.totalClicks = parseInt(state[3]);
-				self.level = parseInt(state[4]);
-				self.isFirstGame = parseInt(state[5]);
-			}
-			if (self.isFirstGame == 1) {
-				$('#instructions').modal('show');
-				self.isFirstGame = 0;
-			}
-			self.setupLevel();
-		});
-		
-	}
+    this.beginGame = function () {
+        var res;
+        this.cookh.readCookie(function (csv) {
+            res = csv;
+            if (res != "") {
+                var state = res.split(" ");
+                self.currentClicks = parseInt(state[0]);
+                self.bestLevel = parseInt(state[1]);
+                self.clicksForBest = parseInt(state[2]);
+                self.totalClicks = parseInt(state[3]);
+                self.level = parseInt(state[4]);
+                self.isFirstGame = parseInt(state[5]);
+            }
+            if (self.isFirstGame == 1) {
+                window.showMessage(window.i18n.__("instruct"), window.i18n.__("instruct_details"), null, null);
+                self.isFirstGame = 0;
+            }
+            self.setupLevel();
+        });
 
-	this.gameEnd = function(callback) {
-		this.level++;
-		if (this.level == this.bestLevel && this.currentClicks < this.clicksForBest) {
-			this.clicksForBest = this.currentClicks;
-		} 
-		if (this.level > this.bestLevel) {
-			this.clicksForBest = this.currentClicks;
-			this.bestLevel = this.level;
-		}
-		this.resetGame();
-		callback();
-	}
+    }
 
-	this.resetGame = function() {
-		$('#cheesyGoodJob').html(this.cwg.getWord()+"!");
-		$('#levelDescriptor').html("进入级别 " + this.level);
-		setTimeout(function(){
-			$('#newLevel').modal('show');
-			self.setupLevel();
-		}, 500);
-		setTimeout(function(){
-			$('#newLevel').modal('hide');
-		}, 1500);
-	}
+    this.gameEnd = function (callback) {
+        this.level++;
+        if (this.level == this.bestLevel && this.currentClicks < this.clicksForBest) {
+            this.clicksForBest = this.currentClicks;
+        }
+        if (this.level > this.bestLevel) {
+            this.clicksForBest = this.currentClicks;
+            this.bestLevel = this.level;
+        }
+        this.resetGame();
+        callback();
+    }
 
-	this.setupLevel = function() {
-		this.gb = new GameBoard(this.level, this.level);
-		$('.board').html("");
-		this.gb.populate();
-		self.gb.renderBoard();
-		self.sh.setGridSize(this.level);
-		self.updateCounts();
-		self.applyBindings();
-	}
+    this.resetGame = function () {
+        $('#cheesyGoodJob').html(this.cwg.getWord() + "!");
+        $('#levelDescriptor').html("进入级别 " + this.level);
+        setTimeout(function () {
+            $('#newLevel').modal('show');
+            self.setupLevel();
+        }, 500);
+        setTimeout(function () {
+            $('#newLevel').modal('hide');
+        }, 1500);
+    }
 
-	this.updateCounts = function() {
-		$(".currLevel").html("当前级别: <b>" + this.level + "</b>");
-		$(".score").html("当前点击次数: <b>" + this.currentClicks +"</b>");
-		$(".best").html("历史最高级别: <b>" + this.bestLevel + "</b> (" + this.clicksForBest + " clicks)");
-		$(".total").html("总计点击次数: <b>" + this.totalClicks + "</b>");
-	}
+    this.setupLevel = function () {
+        this.gb = new GameBoard(this.level, this.level);
+        $('.board').html("");
+        this.gb.populate();
+        self.gb.renderBoard();
+        self.sh.setGridSize(this.level);
+        self.updateCounts();
+        self.applyBindings();
+    }
 
-    this.applyBindings = function() {
-		$('.gamesquare').click(function() {
+    this.updateCounts = function () {
+        $(".currLevel").html(window.i18n.__("current_level") + " : <b>" + this.level + "</b>");
+        $(".score").html(window.i18n.__("current_click_counts") + " : <b>" + this.currentClicks + "</b>");
+        $(".best").html(window.i18n.__("best_ever_level") + " : <b>" + this.bestLevel + "</b> (" + this.clicksForBest + " clicks)");
+        $(".total").html(window.i18n.__("total_click_counts") + " : <b>" + this.totalClicks + "</b>");
+    }
+
+    this.applyBindings = function () {
+        $('.gamesquare').click(function () {
             var corrd = self.getCorrd(this);
             self.processClick(corrd.x, corrd.y);
         });
-        $('.gamesquare').mouseover(function(){
+        $('.gamesquare').mouseover(function () {
             var corrd = self.getCorrd(this);
             self.processMouseover(corrd.x, corrd.y);
         });
-        $('.gamesquare').mouseout(function(){
+        $('.gamesquare').mouseout(function () {
             var corrd = self.getCorrd(this);
             self.processMouseout(corrd.x, corrd.y);
         });
-	}
+    }
 
-	this.onNewGameClick = function() {
-		this.currentClicks = 0;
-		this.level = 1;
-		this.setupLevel();
-	}
+    this.onNewGameClick = function () {
+        this.currentClicks = 0;
+        this.level = 1;
+        this.setupLevel();
+    }
 
-	this.onResetLevelClick = function() {
-		this.gb.populate();
-		this.gb.renderBoard();
-		this.setupLevel();
-	}
+    this.onResetLevelClick = function () {
+        this.gb.populate();
+        this.gb.renderBoard();
+        this.setupLevel();
+    }
 
-    this.getCorrd = function(context) {
+    this.getCorrd = function (context) {
         var cname = $(context).context.className.split(" ")[1];
         var coord = cname.substring(5).split("q");
         var height = parseInt(coord[1]);
@@ -202,50 +202,50 @@ function Game() {
     }
 }
 
-function GameBoard (wd, hi) {
-	// wide and high are 0 indexed
-	this.high = hi - 1;
-	this.wide = wd - 1;
+function GameBoard(wd, hi) {
+    // wide and high are 0 indexed
+    this.high = hi - 1;
+    this.wide = wd - 1;
 
-	this.count = 0;
+    this.count = 0;
 
     this.shake_css = "shake shake-slow shake_hover shake-slow_hover";
 
-	// This board is accessed wide first then high
-	//    0 | 1 | 2 | 3 | ....
+    // This board is accessed wide first then high
+    //    0 | 1 | 2 | 3 | ....
     //  - - - - - - - - - - - -
-   	//  0   |   |   |   | 
-	//  - - - - - - - - - - - -
-	//  1   |   |[2][1]
-	//  -
-	//  2
-	//  :
-	//  :
-	//
-	this.board = new Array(wd);
-	for (var i = 0; i <= this.wide; i++) {
-		this.board[i] = new Array(hi);
-	}
+    //  0   |   |   |   |
+    //  - - - - - - - - - - - -
+    //  1   |   |[2][1]
+    //  -
+    //  2
+    //  :
+    //  :
+    //
+    this.board = new Array(wd);
+    for (var i = 0; i <= this.wide; i++) {
+        this.board[i] = new Array(hi);
+    }
 
-	this.renderBoard = function() {
-		var s = "";
-		for (var j = 0; j <= this.high; j++) {
-			s += "<div class='gamerow'>";
-			for (var i = 0; i <= this.wide; i++) {
-				s += "<div class='gamesquare coord" + i + "q" + j + "'></div>";
-				// console.log(this.board[i][j]);
-			}
-			s += "</div>";
-		}
-		$('.board').html(s);
+    this.renderBoard = function () {
+        var s = "";
+        for (var j = 0; j <= this.high; j++) {
+            s += "<div class='gamerow'>";
+            for (var i = 0; i <= this.wide; i++) {
+                s += "<div class='gamesquare coord" + i + "q" + j + "'></div>";
+                // console.log(this.board[i][j]);
+            }
+            s += "</div>";
+        }
+        $('.board').html(s);
 
 
-		for (var i = 0; i <= this.wide; i++) {
-			for (var j = 0; j <= this.high; j++) {
-				this.processCLickView(i, j);
-			}
-		}
-	}
+        for (var i = 0; i <= this.wide; i++) {
+            for (var j = 0; j <= this.high; j++) {
+                this.processCLickView(i, j);
+            }
+        }
+    }
 
     function getFixedXY(x, y) {
         var lowx = x - 1;
@@ -261,7 +261,7 @@ function GameBoard (wd, hi) {
         return {lowx: lowx, highx: highx, lowy: lowy, highy: highy};
     }
 
-    this.processClick = function(w, h) {
+    this.processClick = function (w, h) {
         // find the proper range for inversion
         var __ret = getFixedXY.call(this, w, h);
         var lowx = __ret.lowx;
@@ -295,7 +295,7 @@ function GameBoard (wd, hi) {
         }
     }
 
-    this.processMouseover = function(w, h) {
+    this.processMouseover = function (w, h) {
         var __ret = getFixedXY.call(this, w, h);
         var lowx = __ret.lowx;
         var highx = __ret.highx;
@@ -313,7 +313,7 @@ function GameBoard (wd, hi) {
         }
     }
 
-    this.processMouseout = function(w, h) {
+    this.processMouseout = function (w, h) {
         var __ret = getFixedXY.call(this, w, h);
         var lowx = __ret.lowx;
         var highx = __ret.highx;
@@ -331,17 +331,17 @@ function GameBoard (wd, hi) {
         }
     }
 
-	// For a single tile finds the corresponding DOM element 
-	// and inverts the color
-	this.processCLickView = function(w, h) {
-		var coord = ".coord" + w + "q" + h;
-		// console.log(coord);
-		if (this.board[w][h] == 0) {
-			$(coord).css("background-color", "#E6AB5E");
-		} else {
-			$(coord).css("background-color", "#5C90FF");
-		}
-	}
+    // For a single tile finds the corresponding DOM element
+    // and inverts the color
+    this.processCLickView = function (w, h) {
+        var coord = ".coord" + w + "q" + h;
+        // console.log(coord);
+        if (this.board[w][h] == 0) {
+            $(coord).css("background-color", "#E6AB5E");
+        } else {
+            $(coord).css("background-color", "#5C90FF");
+        }
+    }
 
     this.processMouseoverView = function (w, h) {
         var coord = ".coord" + w + "q" + h;
@@ -353,26 +353,26 @@ function GameBoard (wd, hi) {
         $(coord).removeClass(this.shake_css);
     }
 
-	// Populate the game board with 0s and 1s randomly
-	this.populate = function() {
-		for (var i = 0; i <= this.wide; i++) {
-			for (var j = 0; j <= this.high; j++) {
-				this.board[i][j] = 0;
-			}
-		}
-	}
+    // Populate the game board with 0s and 1s randomly
+    this.populate = function () {
+        for (var i = 0; i <= this.wide; i++) {
+            for (var j = 0; j <= this.high; j++) {
+                this.board[i][j] = 0;
+            }
+        }
+    }
 
-	this.isGameWin = function() {
-		return this.count == (this.wide + 1) * (this.high + 1);
-	}
+    this.isGameWin = function () {
+        return this.count == (this.wide + 1) * (this.high + 1);
+    }
 
-	/*this.parseGameBoard = function(csv, callback) {
-		var res = csv.split(",");
-		//console.log(res.length);
-		for (var i = 0; i < res.length; i++) {
-			console.log(Math.floor(i/(this.high+1)) + "," + i % (this.high+1));
-			this.board[Math.floor(i / (this.high+1))][i % (this.high+1)] = parseInt(res[i]);
-		}
-		callback();
-	}*/
+    /*this.parseGameBoard = function(csv, callback) {
+     var res = csv.split(",");
+     //console.log(res.length);
+     for (var i = 0; i < res.length; i++) {
+     console.log(Math.floor(i/(this.high+1)) + "," + i % (this.high+1));
+     this.board[Math.floor(i / (this.high+1))][i % (this.high+1)] = parseInt(res[i]);
+     }
+     callback();
+     }*/
 }
